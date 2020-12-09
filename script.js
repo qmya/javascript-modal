587f4cd094acc000b1d7d36f47cb29a8d856b73e
qmya <qandeelgul90@gmail.com>
2020-12-09 16:31:56 -0500

    added an escape functionality to close the modal

--- script.js
+++ script.js
@@ -4,16 +4,12 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnOpenModal);
 
 for (let i = 0; i < btnOpenModal.length; i++) {
-  console.log(
-    btnOpenModal[i].addEventListener('click', function () {
-      console.log(`button ${i + 1} is clicked to open the modal`);
-      modal.classList.remove('hidden');
-      overlay.classList.remove('hidden');
-    })
-  );
+  btnOpenModal[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
 }
 
 //adding event listening to button close
@@ -36,3 +32,13 @@ const closeModal = function () {
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+//hitting the ␛ key to close the modal
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+  if (e.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  }
+});
